b92a816e696776491b5e2092012ee2a0438803de
lordzeus <ansullm10uk@gmail.com>
2025-08-07 14:16:09 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,18 +5,9 @@ import HomePage from './pages/HomePage';
 import React from 'react';
 import LoginPage from './pages/auth/LoginPage';
 import Signup from './pages/auth/Signup';
-import ForgotPassword from './pages/ForgotPassword';
-import OtpVerification from './pages/OtpVerification';
+
 import InstructorDashboard from './pages/instructor/InstructorDashboard';
-import AccountSecurityPage from './pages/newprofilepg/AccountSecurityPage';
-import ProfilePage from './pages/newprofilepg/ProfilePage';
-import PrivacyPage from './pages/newprofilepg/PrivacyPage';
-import AcademicInformationPage from './pages/newprofilepg/AcademicInformationPage';
-import CloseAccountPage from './pages/newprofilepg/CloseAccountPage';
-import NotificationPreferencesPage from './pages/newprofilepg/NotificationPreferencesPage';
-import PaymentMethodsPage from './pages/newprofilepg/PaymentMethodsPage';
-import PhotoPage from './pages/newprofilepg/PhotoPage';
-import SubscriptionsPage from './pages/newprofilepg/SubscriptionsPage';
+
 import Courses from './pages/instructor/Courses';
 import CreateCourse from './pages/instructor/CreateCourse';
 

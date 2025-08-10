1612677d3ddbc8ccdb79f675531713be1e3824d2
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-08-10 19:29:38 +0530

    done

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,104 @@
 import {User} from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const 
+
+export const register = async(req, res) => {
+    try{
+        const {name , email ,password , role}= req.body;
+
+        if(!name || !email || !password || !role){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            });
+        }
+         const hashedPassword = await bcrypt.hash(password,10);
+
+        await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            role
+
+        })
+        return res.status(201).json({
+            success: true,
+            message: "User registered successfully"
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to register user"
+        });
+    }
+}
+
+export const login = async(req ,res)=>{
+    try{
+          const {email, password} = req.body;
+
+          if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+    }
+            const user = await User.findOne({email})
+            if(!user){
+                return res.status(400).json({
+                    success: false,
+                    message: "Invalid email or password"
+                });
+            }
+
+            const isPasswordMatch =await bcrypt.compare(password,user.password);
+            if(!isPasswordMatch){
+                return res,status(400).json({
+                    success: false,
+                    message: "Invalid email or password"
+                })
+            }
+            const token = jwt.sign({userId: user._id},process.env.SECRET_KEY, {expiresIn:"1d"});
+            return res.cookie('token',token,{
+                httpOnly: true,
+                sameSite: 'strict',
+                maxAge: 1*24*60*60*1000 // 1 day
+            }).json({
+                success: true,
+                message: `Welcome back ${user.name}`,
+                user
+            })
+    } catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to login user"
+        });
+    }
+}
+
+export const logout = async(req ,res)=>{
+    try{
+        return res.status(200).cookie('tooken','',{maxAge:0}).json({
+            success: true,
+            message: "User logged out successfully"
+        })
+    } catch{error}{
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to logout user"
+        });
+
+    }
+}
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import { login, logout, register} from '../controllers/authController.js';
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.route('/register').post(register);
 
-module.exports = router;
+router.route('/login').post(login);
+router.route('/logout').get(logout)
+// router.post('/login', login);
+export default router;

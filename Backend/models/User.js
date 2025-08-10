a992e891c65e8af562a05804b3ad160602e5be57
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-08-10 20:39:15 +0530

    login register Api is active

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type:String,
-        enum:["instructor", "student"],
+        enum:["instructor", "student", "admin"],
         required:true
     },
     photoUrl:{

91062a03cc7a40c928dbe998dae0478eb67c3f03
lordzeus05 <ansullm10uk@gmail.com>
2025-08-19 18:06:20 +0530

    ui hi badal dala

--- Frontend/src/pages/courseplayer/CourseHeader.jsx
+++ Frontend/src/pages/courseplayer/CourseHeader.jsx
@@ -7,29 +7,30 @@ function CourseHeader({ title, progress, onResetProgress, onToggleSidebar }) {
   return (
     <header className="p-4 bg-[#E3F1F1] border-b border-gray-200 flex-shrink-0">
       <div className="flex justify-between items-center flex-wrap gap-2">
-        <h1 className="text-xl font-bold text-gray-800">{title}</h1>
+        <a
+            href="#dashboard"
+            className="text-lg font-medium text-gray-600 hover-text-indigo-600" // <-- classes removed here
+          >
+            Dashboard
+          </a>
+        <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
 
         <div className="flex items-center gap-4">
-          <a
-            href="#dashboard"
-            className="text-lg font-medium text-gray-600 hover-text-indigo-600 hidden md:block"
-          >
-            Back to Dashboard
-          </a>
+          
           <button
             onClick={onToggleSidebar}
             className="text-gray-600 hover-text-indigo-600 md:hidden"
             aria-label="Toggle sidebar"
           >
-            <RxHamburgerMenu className="text-3xl"/>
+            <RxHamburgerMenu className="text-3xl" />
           </button>
         </div>
       </div>
-      <div className="flex justify-start i  tems-center gap-3 mt-2">
+      <div className="flex justify-end items-center gap-3 mt-2">
         <div className="w-48 h-2 bg-[#00173D] rounded-full overflow-hidden">
           <div
             className="h-full bg-[#006D77]"
-            style={{ width: `${progress}%` }} 
+            style={{ width: `${progress}%` }}
           ></div>
         </div>
         <span className="text-sm font-medium text-gray-600 w-25 text-right">
@@ -40,4 +41,4 @@ function CourseHeader({ title, progress, onResetProgress, onToggleSidebar }) {
   );
 }
 
-export default CourseHeader;
+export default CourseHeader;  

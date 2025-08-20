3418dbb31a1ab2a71775e5a9b1979d22b325f203
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-08-20 12:15:23 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,6 +16,7 @@ import Courses from "./pages/instructor/Courses";
 import CreateCourse from "./pages/instructor/CreateCourse";
 import UpdateCourse from "./pages/instructor/UpdateCourse";
 import CreateLecture from "./pages/instructor/CreateLecture";
+import EditLecture from "./pages/instructor/EditLecture";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
         <Route path="course/create" element={<CreateCourse />} />
         <Route path="course/:courseId" element={<UpdateCourse />} />
         <Route path="course/:courseId/lecture" element={<CreateLecture />} />
+        <Route path="course/:courseId/lecture/:lectureId" element={<EditLecture />} />
       </Route>
 
       <Route path="/profile" element={<ProfilePage />} />

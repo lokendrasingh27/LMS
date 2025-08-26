1a1c600d3cb176eecf0b1f963539b5cdbcc61b83
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-08-26 13:21:17 +0530

    done

--- Frontend/src/pages/instructor/EditLecture.jsx
+++ Frontend/src/pages/instructor/EditLecture.jsx
@@ -10,11 +10,17 @@ const EditLecture = () => {
   return (
     <div className='p-4 md:p-10 h-screen'>
         <div className='flex items-center justify-between mb-5'>
-            <div className='flex items-center gap-2'>
-                <Link to={`/instructor/course/${courseId}/lecture`}>
+            <div className='flex items-center justify-between w-full '>
+                <div className='flex items-center  gap-2'>
+                  <Link to={`/instructor/course/${courseId}/lecture`}>
                 <Button size="icon" variant="outline" className='rounded-full'><ArrowBigLeft size={16}/></Button>
                 </Link>
                 <h1 className='font-bold text-xl text-[#001F3F]'>Update Your Lecture</h1>
+                </div>
+                <div className=' flex gap-2'>
+                  <Button className='bg-[#001F3F] hover:bg-[#006D77]'>Create Assignment</Button>
+                  <Button className='bg-[#001F3F] hover:bg-[#006D77]'>Create Quizess</Button>
+                </div>
             </div>
         </div>
        <LectureTab/>

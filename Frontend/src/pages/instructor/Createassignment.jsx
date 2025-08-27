cff1b39bbc28c4c9a0ddf8682572280ec7895d77
lordzeus05 <ansullm10uk@gmail.com>
2025-08-27 15:09:13 +0530

    enchance

--- Frontend/src/pages/instructor/Createassignment.jsx
+++ Frontend/src/pages/instructor/Createassignment.jsx
@@ -10,9 +10,6 @@ const FileUpIcon = () => (
 const XIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
 );
-const StarIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-);
 
 
 const CreateAssignment = ({ isOpen, onClose }) => {

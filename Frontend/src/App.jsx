2e6988e2af7c9e4d1ef89035e207a6af17ac1e73
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-09-11 14:12:48 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,6 @@ import EnrolledDashboard from "./pages/student/EnrolledDashboard";
 import EditLecture from "./pages/instructor/EditLecture";
 import PaymentHistory from "./pages/student/PaymentHistory";
 import AssignmentGradingPage from "./pages/instructor/AssignmentGradingPage";
-<<<<<<< HEAD
 import CertificatePage from "./pages/student/Certificatepage";
 import Dashboard from './pages/admin/Dashboard';
 import Instructors from './pages/admin/Instructors';
@@ -28,9 +27,6 @@ import AssessmentResults from "./pages/admin/AssessmentResults";
 import Communications from './pages/admin/Communications';
 import FinancialManagement from './pages/admin/FinancialManagement';
 import ChatBox from "./pages/Chatbox";
-=======
-// import CertificatePage from "./pages/student/Certificatepage";
->>>>>>> 6ac77445c335aaf84d55a8e5c8d0ea1fba524773
 // import CertificatePage from "./pages/student/Certificatepage";
 
 

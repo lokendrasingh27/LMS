0cc7438df90416b6e8fe7eea0262c896078dd18a
manjarigoyal16 <manjarigoyal2616@gmail.com>
2025-09-13 22:54:19 +0530

    admin

--- Frontend/src/pages/admin/AdminDemoDashboard.jsx
+++ Frontend/src/pages/admin/AdminDemoDashboard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import AdminSidebar from "../../components/AdminSidebar";
+import Sidebar from "@/components/Sidebar";
+
 
 const AdminDemoDashboard = () => {
   return (
     <div className="flex min-h-screen">
-      <AdminSidebar />
+      <Sidebar />
       <main className="flex-1 p-6 bg-gray-100">
         <Outlet /> {/* renders the matching child route here */}
       </main>

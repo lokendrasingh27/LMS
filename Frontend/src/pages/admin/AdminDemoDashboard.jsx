219b567331646fdf6ad9bf18f8e5e3397cbd5eda
manjarigoyal16 <manjarigoyal2616@gmail.com>
2025-09-19 23:39:53 +0530

    .

--- Frontend/src/pages/admin/AdminDemoDashboard.jsx
+++ Frontend/src/pages/admin/AdminDemoDashboard.jsx
@@ -5,14 +5,13 @@ import Sidebar from "../../components/Sidebar";
 
 const AdminDemoDashboard = () => {
   return (
-   <div className='bg-[#e3f1f1] h-[100vh] lg:flex relative min-h-screen  overflow-hidden w-'>
-      <Sidebar/>
-      <div className='flex-1  '>
-        <Outlet/>
+    <div className="bg-[#e3f1f1] h-[100vh] lg:flex relative min-h-screen">
+      <Sidebar />
+      <div className="flex-1 overflow-auto">
+        <Outlet />
       </div>
     </div>
   );
 };
 
 export default AdminDemoDashboard;
-
